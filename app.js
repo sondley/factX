82f9ef13ab9adc08cf8f3fa651d0ae19aa912bd0
sondley <32400754+sondley@users.noreply.github.com>
2018-12-03 17:51:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -136,8 +136,8 @@ app.use(function(err, req, res, next) {
 // listen on port 3000
 //models.sequelize.sync(({ force: true })).then(() => {
 models.sequelize.sync().then(() => {
-  app.listen(process.env.PORT || 3000, function () {
-  console.log('Express app listening on port 3000');
+  app.listen(process.env.PORT || 5000, function () {
+  console.log('Express app listening on port 5000');
 })
 });
 
